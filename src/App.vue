8edd7ff75e28519b241a2b86177f1273382f2472
stefan1 <>
2023-03-27 08:50:30 +0200

    testing my git action

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/products">Products</RouterLink>
+        <RouterLink to="/converter">Converter</RouterLink>
       </nav>
     </div>
   </header>
